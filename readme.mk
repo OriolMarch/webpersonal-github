9458acae89dc2415964cf6f658a07d848b370027
oriolmarch <icewoo2001@gmail.com>
2025-09-30 20:02:18 +0200

    Readme + HTML inicial

---
+++ readme.mk
@@ -0,0 +1,4 @@
+En aquesta activitat desenvoluparem una web molt bàsica, aprendrem a gestionar el github i la compartició de recursos.
+Es important aquesta activitat, ja que el GitHub es una eina molt utilitzada a nivell empreserial i per tindré un control de versions, adecuat i organitzat.
+
+att: Oriol March Vernet.
